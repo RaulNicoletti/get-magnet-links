72e9e557dec5d48c22251c29cbcf4127a9532895
Raul Nicoletti <raulnicoletti.dev@gmail.com>
2020-11-29 19:05:56 -0300

    feat(header): add header and footer implementantion

--- src/views/Popup/components/Footer.tsx
+++ src/views/Popup/components/Footer.tsx
@@ -1,6 +1,43 @@
+import Paper from '@material-ui/core/Paper';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      alignItems: 'center',
+      alignContent: 'center',
+      flexGrow: 1,
+      height: '40px',
+      position: 'sticky',
+      bottom: 0,
+    },
+    typography: {
+      marginLeft: theme.spacing(1),
+      display: 'flex',
+      alignItems: 'center',
+      alignContent: 'center',
+    },
+    icon: {
+      marginLeft: theme.spacing(1),
+    }
+  }),
+);
+
 const Footer: React.FC = () => {
+  const classes = useStyles();
+  const github = 'https://github.com/RaulNicoletti';
+  const handleClick = () => window.open(github);
+
   return (
-    <p>Footer</p>
+    <Paper className={classes.root} elevation={10}>
+      <Typography className={classes.typography} variant="caption" color="inherit">
+        See on github
+        <GitHubIcon className={classes.icon} titleAccess={github} onClick={handleClick} cursor="pointer" />
+      </Typography>
+    </Paper>
   )
 }
 
--- src/views/Popup/components/Header.tsx
+++ src/views/Popup/components/Header.tsx
@@ -1,7 +1,38 @@
+import React from 'react';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      minWidth: '200px',
+      marginBottom: '80px',
+
+    },
+    appBar: {
+      height: '60px',
+      position: 'fixed',
+    },
+  }),
+);
+
 const Header: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <p>Header</p>
-  )
+    <div className={classes.root}>
+      <AppBar className={classes.appBar}>
+        <Toolbar variant="regular">
+          <Typography variant="subtitle1" color="inherit">
+            Get Magnet Links
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export { Header };
