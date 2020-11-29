b0ce947de5675e48516b6ad36ba296bf4b03bed9
Raul Nicoletti <raulnicoletti.dev@gmail.com>
2020-11-29 17:22:40 -0300

    refactor(list): split logic from the component

--- src/views/Popup/index.tsx
+++ src/views/Popup/index.tsx
@@ -1,26 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { browser } from 'webextension-polyfill-ts';
-import '../App.css';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { List } from './components/List';
+import { sendMessage } from './services';
 
 const Popup: React.FC = () => {
   const [links, setLinks] = useState<string[]>([]);
 
   useEffect(() => {
-    async function query() {
-      try {
-        const tabs = await browser.tabs.query({ active: true, currentWindow: true });
-        const response: string[] = await browser.tabs.sendMessage(tabs[0].id!, '');
-        setLinks(response);
-      } catch (err) {
-        if (browser.runtime.lastError) setLinks([]);
-        else console.log(err);
-      }
+    async function getLinks() {
+      const response = await sendMessage();
+      setLinks(response);
     }
 
-    query();
+    getLinks();
   }, []);
 
   return (
--- src/views/Popup/services.ts
+++ src/views/Popup/services.ts
@@ -1,5 +1,21 @@
-function useLinks() {
-  console.log('')
+import { browser } from 'webextension-polyfill-ts';
+
+async function sendMessage() {
+  let links: string[] = [];
+  try {
+    const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+    links = await browser.tabs.sendMessage(tabs[0].id!, '');
+  } catch (err) {
+    if (browser.runtime.lastError) {
+      console.log(browser.runtime.lastError.message);
+    } else {
+      console.log(err);
+    }
+
+    return links;
+  }
+
+  return links;
 }
 
-export { useLinks };
+export { sendMessage };
